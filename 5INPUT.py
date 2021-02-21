e7a055c519a37726201e5db50b7995743f1b784a
sandeep kumar chaurasia <58679551+sandeepkrc@users.noreply.github.com>
2021-02-21 11:15:47 +0530

    Add files via upload

---
+++ 5INPUT.py
@@ -0,0 +1,15 @@
+
+#input data is always taking as a string
+#no matter what type of data you enter
+x=input("please enter the string= ")
+print(type(x))
+y=input("please enter int no = ")
+print(type(y))
+z=input("please enter float no = ")
+print(type(z))
+a=input("please enter bool = ")
+print(type(a))
+b=input("please enter list = ")
+print(type(b))
+#we can solve this problem by using type
+#conversion function (if it is possible)
